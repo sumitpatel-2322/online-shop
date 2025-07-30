3dc548829769562f4887f5cd5f235253b1495a6a
Sumit Patel <sumitpatel2907@gmail.com>
2025-07-30 21:33:48 +0530

    Made some changes

--- config/session.js
+++ config/session.js
@@ -1,23 +1,30 @@
-const expressSession=require('express-session');
-const mongoDbStore=require('connect-mongodb-session');
-function createSessionStore(){
-    const MongoDBStore=mongoDbStore(expressSession);
-    const store=new MongoDBStore({
-        uri:'mongodb://localhost:27017',
-        databaseName:'online-shop',
-        collection:'sessions'
-    });
-    return store;
+const expressSession = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(expressSession);
+
+function createSessionStore() {
+  const store = new MongoDBStore({
+    uri: process.env.MONGODB_URI,
+    databaseName: 'online-shop',
+    collection: 'sessions'
+  });
+
+  store.on('error', function(error) {
+    console.error('Session store error:', error);
+  });
+
+  return store;
 }
-function createSessionConfig(){
-    return{
-        secret:'super-secret',
-        resave:false,
-        saveUninitialized:false,
-        store:createSessionStore(),
-        cookie:{
-            maxAge:2*24*60*60*1000
-        }
-    };
+
+function createSessionConfig() {
+  return {
+    secret: 'super-secret',     // Use a strong secret, ideally stored in env variables
+    resave: false,
+    saveUninitialized: false,
+    store: createSessionStore(),
+    cookie: {
+      maxAge: 2 * 24 * 60 * 60 * 1000 // 2 days
+    }
+  };
 }
-module.exports=createSessionConfig;
+
+module.exports = createSessionConfig;
